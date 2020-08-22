67b714ca892d19620df72630ab287fa421d1ee50
LAPTOP-FP5OC7O8\irfan <erfan.mbkk@gmail.com>
2020-08-22 14:22:14 +0700

    get start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48568e5c6a893293f1166898026bda80'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '849c5de2-640f-4619-aa61-f12efca0748c'
+};
